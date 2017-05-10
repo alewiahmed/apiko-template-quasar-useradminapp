ddd4db4db6fe730781ae640c476bdf48e4eaf604
Alewi Ahmed <alewimail@gmail.com>
2017-05-10 15:38:53 +0300

    Initial Commit

---
+++ src/components/Index.vue
@@ -0,0 +1,109 @@
+<template>
+  <q-layout>
+    <div slot="header" class="toolbar">
+      <q-toolbar-title :padding="0">
+        Quasar Framework v{{$q.version}}
+      </q-toolbar-title>
+    </div>
+
+    <!--
+      Replace following "div" with
+      "<router-view class="layout-view">" component
+      if using subRoutes
+    -->
+    <div class="layout-view">
+      <div class="logo-container non-selectable no-pointer-events">
+        <div class="logo" :style="position">
+          <img src="~assets/quasar-logo.png">
+          <p class="caption text-center">
+            <span v-if="orienting">Tilt your device.</span>
+            <template v-else>
+              <span class="desktop-only">Move your mouse.</span>
+              <span class="touch-only">Touch screen and move.</span>
+            </template>
+          </p>
+        </div>
+      </div>
+    </div>
+  </q-layout>
+</template>
+
+<script>
+var moveForce = 30
+var rotateForce = 40
+
+import { Utils, Platform } from 'quasar'
+
+export default {
+  data () {
+    return {
+      orienting: window.DeviceOrientationEvent && !Platform.is.desktop,
+      moveX: 0,
+      moveY: 0,
+      rotateY: 0,
+      rotateX: 0
+    }
+  },
+  computed: {
+    position () {
+      let transform = `rotateX(${this.rotateX}deg) rotateY(${this.rotateY}deg)`
+      return {
+        top: this.moveY + 'px',
+        left: this.moveX + 'px',
+        '-webkit-transform': transform,
+        '-ms-transform': transform,
+        transform
+      }
+    }
+  },
+  methods: {
+    move (evt) {
+      const {width, height} = Utils.dom.viewport()
+      const {top, left} = Utils.event.position(evt)
+      const halfH = height / 2
+      const halfW = width / 2
+
+      this.moveX = (left - halfW) / halfW * -moveForce
+      this.moveY = (top - halfH) / halfH * -moveForce
+      this.rotateY = (left / width * rotateForce * 2) - rotateForce
+      this.rotateX = -((top / height * rotateForce * 2) - rotateForce)
+    },
+    orient (evt) {
+      this.rotateX = evt.beta * 0.7
+      this.rotateY = evt.gamma * -0.7
+    }
+  },
+  mounted () {
+    this.$nextTick(() => {
+      if (this.orienting) {
+        window.addEventListener('deviceorientation', this.orient, false)
+      }
+      else {
+        document.addEventListener('mousemove', this.move)
+      }
+    })
+  },
+  beforeDestroy () {
+    if (this.orienting) {
+      window.removeEventListener('deviceorientation', this.orient, false)
+    }
+    else {
+      document.removeEventListener('mousemove', this.move)
+    }
+  }
+}
+</script>
+
+<style lang="styl">
+.logo-container
+  width 192px
+  height 268px
+  perspective 800px
+  position absolute
+  top 50%
+  left 50%
+  transform translateX(-50%) translateY(-50%)
+.logo
+  position absolute
+  transform-style preserve-3d
+</style>
